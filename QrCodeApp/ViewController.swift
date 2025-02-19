8caa6bea018108e856fb79de00ca1d764d5bffbd
Rümeysa Tokur <rtokur11@gmail.com>
2025-02-20 01:58:33 +0300

    Initial Commit

---
+++ QrCodeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QrCodeApp
+//
+//  Created by Rumeysa Tokur on 20.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
